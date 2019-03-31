eb5a3f4e1d064e8d38f8d35625e8ea7a57bae228
Chance Hudson <jchancehud@gmail.com>
2019-03-31 09:14:38 -0500

    Refactor class to clean js

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 export default class Hydrated {
-  static stores = {}
-  static __hydrating = false
-
-  static async hydrate(...args: string[]) {
-    if (this.__hydrating) return
-    try {
-      const promises = []
-      for (const key in this.stores) {
-        if (args.length && args.indexOf(key) !== -1) continue
-        promises.push(this.stores[key].hydrate())
-      }
-      await Promise.all(promises)
-    } catch (err) {
-      console.log('Error hydrating stores')
-    }
-  }
-
   hydrate() {
     throw new Error('Override hydrate() in hydrated subclasses')
+  }
+}
+
+Hydrated.stores = {}
+Hydrated.__hydrating = false
+
+Hydrated.hydrate = async (...args) => {
+  if (Hydrated.__hydrating) return
+  try {
+    const promises = []
+    for (const key in Hydrated.stores) {
+      if (args.length && args.indexOf(key) !== -1) continue
+      promises.push(Hydrated.stores[key].hydrate())
+    }
+    await Promise.all(promises)
+  } catch (err) {
+    console.log('Error hydrating stores')
   }
 }
